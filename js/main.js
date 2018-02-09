a68cac75bde089c50bd5ba611b1333e1f92d0b1a
brayanlopez8 <brayanlopez8@gmail.com>
2018-02-09 16:41:29 -0500

    Ajuste de preloader

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 	///////////////////////////
 	// Preloader
 	$(window).on('load', function() {
-		$("#preloader").delay(600).fadeOut();
+		$("#preloader").delay(10).fadeOut();
 	});
 
 	///////////////////////////
